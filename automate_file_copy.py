66e0e2e5069985453aeaf1bc98af00c8ad6a34cb
Waqas Obeidy <waqas@siswa.ukm.edu.my>
2019-08-19 00:57:01 +0800

    MoveFiles
    SampleScript to move files from folder A-B

---
+++ automate_file_copy.py
@@ -0,0 +1,36 @@
+from watchdog.observers import Observer
+import time
+from watchdog.events import FileSystemEventHandler
+import os
+import json
+
+class MyHandler(FileSystemEventHandler):
+    i=1
+    def on_modified(self, event):
+        for filename in os.listdir(folder_to_track):
+            src = folder_to_track+"/"+filename
+            new_destination = folder_destination+"/"+filename
+            os.rename(src, new_destination)
+
+            
+folder_to_track ='/Users/waqasmcbookair/Desktop/trialFolder'
+folder_destination ='/Users/waqasmcbookair/Desktop/trialFolder2'
+event_handler = MyHandler()
+observer = Observer()
+observer.schedule(event_handler, folder_to_track, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(10)
+except KeyboardInterrupt:
+    observer.stop()
+
+observer.join()
+        
+        
+        
+# if __name__ == "__main__":
+#     print('running')
+    
+    
